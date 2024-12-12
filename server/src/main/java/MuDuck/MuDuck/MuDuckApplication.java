37ee66b3af89b7b5b7083c99d58e1120a18dba6b
hyogeun <stonepagoda8@gmail.com>
2024-12-12 17:25:43 +0900

    fix: 어노테이션 추가

--- server/src/main/java/MuDuck/MuDuck/MuDuckApplication.java
+++ server/src/main/java/MuDuck/MuDuck/MuDuckApplication.java
@@ -2,14 +2,17 @@ package MuDuck.MuDuck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan("MuDuck.MuDuck.domain")  // 엔티티 스캔 위치
+@EnableJpaRepositories("MuDuck.MuDuck.repository")  // 레포지토리 스캔 위치
 public class MuDuckApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MuDuckApplication.class, args);
     }
-
 }
